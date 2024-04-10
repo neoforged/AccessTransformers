c771153b75a02c834e8799977441a77c6428879c
Luke Bemish <lukebemish@lukebemish.dev>
2024-04-09 23:39:31 -0500

    Slight cleanup to make some bits more clear

--- src/main/java/net/neoforged/accesstransformer/parser/AtParser.java
+++ src/main/java/net/neoforged/accesstransformer/parser/AtParser.java
@@ -27,7 +27,7 @@ public final class AtParser {
             List<AccessTransformer> accessTransformers = new ArrayList<>();
             String line;
             while ((line = reader.readLine()) != null) {
-                // Strip comments and split on spaces
+                // Strip comments and split on groups of spaces, avoiding empty parts
                 StringBuilder builder = new StringBuilder();
                 List<String> parts = new ArrayList<>();
                 builder.setLength(0);
@@ -69,12 +69,11 @@ public final class AtParser {
 
                 String className = parts.get(1);
                 // Validate class name as dot-separated java identifiers
-                int finalIndex = reader.getLineNumber();
                 if (className.chars().reduce(0, (last, current) -> {
                     if (last == 0 && !Character.isJavaIdentifierStart(current) ||
                             last != 0 && current != '.' && !Character.isJavaIdentifierPart(current)
                     ) {
-                        throw new RuntimeException("Invalid class name '" + className + "' at line " + finalIndex);
+                        throw new RuntimeException("Invalid class name '" + className + "' at line " + reader.getLineNumber());
                     }
                     return current == '.' ? 0 : 1;
                 }) != 1) {
@@ -88,25 +87,25 @@ public final class AtParser {
                     // Class ATs of inner classes have a corresponding inner class AT for the nesting class
                     locateInnerClassAts(className, modifier, finalState, originName, reader.getLineNumber(), accessTransformers);
                 } else {
-                    String identifier = parts.get(2);
-                    if (identifier.equals("*")) {
+                    String member = parts.get(2);
+                    if (member.equals("*")) {
                         target = new WildcardTarget(className, false);
-                    } else if (identifier.equals("*()")) {
+                    } else if (member.equals("*()")) {
                         target = new WildcardTarget(className, true);
-                    } else if (identifier.contains("(")) {
-                        String name = identifier.substring(0, identifier.indexOf('('));
+                    } else if (member.contains("(")) {
+                        String name = member.substring(0, member.indexOf('('));
                         // For some reason old forge ATs let you use descriptors with dots instead of slashes
                         // We replicate this behavior here
-                        String desc = identifier.substring(identifier.indexOf('('))
+                        String desc = member.substring(member.indexOf('('))
                                 .replace('.', '/');
                         Type methodDescriptor = validateMethodDescriptor(desc, reader.getLineNumber());
                         if (!name.equals("<init>")) {
-                            validateIdentifier(name, "Invalid method name '", reader.getLineNumber());
+                            validateIdentifier(name, "method", reader.getLineNumber());
                         }
                         target = new MethodTarget(className, name, methodDescriptor);
                     } else {
-                        validateIdentifier(identifier, "Invalid field name '", reader.getLineNumber());
-                        target = new FieldTarget(className, identifier);
+                        validateIdentifier(member, "field", reader.getLineNumber());
+                        target = new FieldTarget(className, member);
                     }
                 }
                 accessTransformers.add(new AccessTransformer(target, modifier, finalState, originName, reader.getLineNumber()));
@@ -138,11 +137,11 @@ public final class AtParser {
         return methodDescriptor;
     }
 
-    private static void validateIdentifier(String name, String x, int index) {
+    private static void validateIdentifier(String name, String sort, int index) {
         if (!Character.isJavaIdentifierStart(name.charAt(0)) ||
                 name.chars().skip(1).anyMatch(c -> !Character.isJavaIdentifierPart(c))
         ) {
-            throw new RuntimeException(x + name + "' at line " + index);
+            throw new RuntimeException("Invalid " + sort + "name '" + name + "' at line " + index);
         }
     }
 }
