9eeb0eb9995c197ea58ee32da36d756f0c129a17
Luke Bemish <lukebemish@lukebemish.dev>
2024-04-14 15:16:35 -0500

    Use records

--- parser/src/main/java/net/neoforged/accesstransformer/parser/Target.java
+++ parser/src/main/java/net/neoforged/accesstransformer/parser/Target.java
@@ -1,39 +1,16 @@
 package net.neoforged.accesstransformer.parser;
 
-public sealed abstract class Target {
-    private final String className;
+public sealed interface Target {
+    String className();
 
-    protected Target(String className) {
-        this.className = className;
-    }
-
-    public final String className() {
-        return className;
-    }
-
-    public static final class ClassTarget extends Target {
-        public ClassTarget(String className) {
-            super(className);
-        }
-
+    record ClassTarget(String className) implements Target {
         @Override
         public String toString() {
             return className() + " CLASS";
         }
     }
 
-    public static final class InnerClassTarget extends Target {
-        private final String innerName;
-
-        public InnerClassTarget(String className, String innerName) {
-            super(className);
-            this.innerName = innerName;
-        }
-
-        public String innerName() {
-            return innerName;
-        }
-
+    record InnerClassTarget(String className, String innerName) implements Target {
         @Override
         public String toString() {
             int idx = innerName().lastIndexOf('$');
@@ -41,64 +18,28 @@ public sealed abstract class Target {
         }
     }
 
-    public static final class FieldTarget extends Target {
-        private final String fieldName;
-
-        public FieldTarget(String className, String fieldName) {
-            super(className);
-            this.fieldName = fieldName;
-        }
-
-        public String fieldName() {
-            return fieldName;
-        }
-
+    record FieldTarget(String className, String fieldName) implements Target {
         @Override
         public String toString() {
             return className() + " FIELD " + fieldName();
         }
     }
 
-    public static final class MethodTarget extends Target {
-        private final String methodName;
-        private final String methodDesc;
-
-        public MethodTarget(String className, String methodName, String methodDesc) {
-            super(className);
-            this.methodName = methodName;
-            this.methodDesc = methodDesc;
-        }
-
-        public String methodName() {
-            return methodName;
-        }
-
-        public String methodDescriptor() {
-            return methodDesc;
-        }
-
+    record MethodTarget(String className, String methodName, String methodDescriptor) implements Target {
         @Override
         public String toString() {
             return className() + " METHOD " + methodName() + methodDescriptor();
         }
     }
 
-    public static final class WildcardMethodTarget extends Target {
-        public WildcardMethodTarget(String className) {
-            super(className);
-        }
-
+    record WildcardMethodTarget(String className) implements Target {
         @Override
         public String toString() {
             return className() + " METHODWILDCARD";
         }
     }
 
-    public static final class WildcardFieldTarget extends Target {
-        public WildcardFieldTarget(String className) {
-            super(className);
-        }
-
+    record WildcardFieldTarget(String className) implements Target {
         @Override
         public String toString() {
             return className() + " FIELDWILDCARD";
