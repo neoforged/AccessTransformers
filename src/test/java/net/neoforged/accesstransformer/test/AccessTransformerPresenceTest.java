3f8603395783a80eff86c043aec72c0bda88e8b2
XFactHD <xfacthd@gmx.de>
2024-11-23 22:18:44 +0100

    Fix AT presence test

--- src/test/java/net/neoforged/accesstransformer/test/AccessTransformerPresenceTest.java
+++ src/test/java/net/neoforged/accesstransformer/test/AccessTransformerPresenceTest.java
@@ -7,25 +7,30 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.file.Path;
 import java.util.Set;
 
 public class AccessTransformerPresenceTest {
     @Test
     public void testSourceLookup() throws IOException, URISyntaxException {
         final AccessTransformerList atLoader = new AccessTransformerList();
-        atLoader.loadFromResource("forge_at.cfg");
+
+        Path filePath = Path.of(getClass().getClassLoader().getResource("forge_at.cfg").toURI());
+        atLoader.loadFromPath(filePath);
+
+        String sourcePrefix = filePath.toString();
 
         // Class
-        Assertions.assertEquals(Set.of("forge_at.cfg:99"), atLoader.getSourcesForTarget("net.minecraft.item.crafting.RecipeTippedArrow", TargetType.CLASS, null));
+        Assertions.assertEquals(Set.of(sourcePrefix + ":99"), atLoader.getSourcesForTarget("net.minecraft.item.crafting.RecipeTippedArrow", TargetType.CLASS, null));
         // Inner class
-        Assertions.assertEquals(Set.of("forge_at.cfg:76"), atLoader.getSourcesForTarget("net.minecraft.world.gen.structure.StructureStrongholdPieces$Stronghold", TargetType.CLASS, null));
+        Assertions.assertEquals(Set.of(sourcePrefix + ":76"), atLoader.getSourcesForTarget("net.minecraft.world.gen.structure.StructureStrongholdPieces$Stronghold", TargetType.CLASS, null));
         // Method
-        Assertions.assertEquals(Set.of("forge_at.cfg:6"), atLoader.getSourcesForTarget("net.minecraft.block.Block", TargetType.METHOD, "func_149752_b(F)Lnet/minecraft/block/Block;"));
+        Assertions.assertEquals(Set.of(sourcePrefix + ":6"), atLoader.getSourcesForTarget("net.minecraft.block.Block", TargetType.METHOD, "func_149752_b(F)Lnet/minecraft/block/Block;"));
         // Field
-        Assertions.assertEquals(Set.of("forge_at.cfg:22"), atLoader.getSourcesForTarget("net.minecraft.entity.EntityTrackerEntry", TargetType.FIELD, "field_73134_o"));
+        Assertions.assertEquals(Set.of(sourcePrefix + ":22"), atLoader.getSourcesForTarget("net.minecraft.entity.EntityTrackerEntry", TargetType.FIELD, "field_73134_o"));
         // Method wildcard
-        Assertions.assertEquals(Set.of("forge_at.cfg:29"), atLoader.getSourcesForTarget("net.minecraft.world.biome.Biome", TargetType.METHOD, "this_is_ignored(F)Ldoes/not/Matter;"));
+        Assertions.assertEquals(Set.of(sourcePrefix + ":29"), atLoader.getSourcesForTarget("net.minecraft.world.biome.Biome", TargetType.METHOD, "this_is_ignored(F)Ldoes/not/Matter;"));
         // Field wildcard
-        Assertions.assertEquals(Set.of("forge_at.cfg:51"), atLoader.getSourcesForTarget("net.minecraft.world.biome.BiomeDecorator", TargetType.FIELD, "this_is_ignored"));
+        Assertions.assertEquals(Set.of(sourcePrefix + ":51"), atLoader.getSourcesForTarget("net.minecraft.world.biome.BiomeDecorator", TargetType.FIELD, "this_is_ignored"));
     }
 }
