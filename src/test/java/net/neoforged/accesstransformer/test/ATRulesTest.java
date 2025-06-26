5b335651c624328c6ab0eb0c9f92fc1add3c471d
Matyrobbrt <matyrobbrt@gmail.com>
2025-06-27 00:46:24 +0300

    Test cleanup

--- src/test/java/net/neoforged/accesstransformer/test/ATRulesTest.java
+++ src/test/java/net/neoforged/accesstransformer/test/ATRulesTest.java
@@ -9,7 +9,7 @@ import static net.neoforged.accesstransformer.parser.Transformation.Modifier.*;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ATRulesTest {
+class ATRulesTest {
     @Test
     void testATUpgrade() {
         // Does AT upgrade private->default->protected->public?
@@ -18,29 +18,30 @@ public class ATRulesTest {
         int prot = Opcodes.ACC_PROTECTED; // 0x2
         int pub = Opcodes.ACC_PUBLIC; // 0x1
         assertAll(
-            ()->assertEquals(deflt, AccessTransformer.mergeWith(privte, DEFAULT, LEAVE), "Upgrade private to default"),
-            ()->assertEquals(prot, AccessTransformer.mergeWith(privte, PROTECTED, LEAVE), "Upgrade private to protected"),
-            ()->assertEquals(pub, AccessTransformer.mergeWith(privte, PUBLIC, LEAVE), "Upgrade private to public"),
-            ()->assertEquals(pub, AccessTransformer.mergeWith(pub, PRIVATE, LEAVE), "No downgrade public to private"),
-            ()->assertEquals(pub, AccessTransformer.mergeWith(pub, DEFAULT, LEAVE), "No downgrade public to default"),
-            ()->assertEquals(pub, AccessTransformer.mergeWith(pub, PROTECTED, LEAVE), "No downgrade public to protected"),
-            ()->assertEquals(prot, AccessTransformer.mergeWith(prot, PRIVATE, LEAVE), "No downgrade prot to private"),
-            ()->assertEquals(prot, AccessTransformer.mergeWith(prot, DEFAULT, LEAVE), "No downgrade prot to default"),
-            ()->assertEquals(deflt, AccessTransformer.mergeWith(deflt, PRIVATE, LEAVE), "No downgrade default to private")
+                () -> assertEquals(deflt, AccessTransformer.mergeWith(privte, DEFAULT, LEAVE), "Upgrade private to default"),
+                () -> assertEquals(prot, AccessTransformer.mergeWith(privte, PROTECTED, LEAVE), "Upgrade private to protected"),
+                () -> assertEquals(pub, AccessTransformer.mergeWith(privte, PUBLIC, LEAVE), "Upgrade private to public"),
+                () -> assertEquals(pub, AccessTransformer.mergeWith(pub, PRIVATE, LEAVE), "No downgrade public to private"),
+                () -> assertEquals(pub, AccessTransformer.mergeWith(pub, DEFAULT, LEAVE), "No downgrade public to default"),
+                () -> assertEquals(pub, AccessTransformer.mergeWith(pub, PROTECTED, LEAVE), "No downgrade public to protected"),
+                () -> assertEquals(prot, AccessTransformer.mergeWith(prot, PRIVATE, LEAVE), "No downgrade prot to private"),
+                () -> assertEquals(prot, AccessTransformer.mergeWith(prot, DEFAULT, LEAVE), "No downgrade prot to default"),
+                () -> assertEquals(deflt, AccessTransformer.mergeWith(deflt, PRIVATE, LEAVE), "No downgrade default to private")
         );
     }
+
     @Test
     void testFinalUpgrade() {
         int fnl = Opcodes.ACC_FINAL; // 0x10
         int notfnl = 0;
 
         assertAll(
-                ()->assertEquals(fnl, AccessTransformer.mergeWith(fnl, DEFAULT, MAKEFINAL), "makefinal+final = final"),
-                ()->assertEquals(fnl, AccessTransformer.mergeWith(notfnl, DEFAULT, MAKEFINAL), "makefinal+notfinal = final"),
-                ()->assertEquals(notfnl, AccessTransformer.mergeWith(fnl, DEFAULT, REMOVEFINAL), "removefinal+final = notfinal"),
-                ()->assertEquals(notfnl, AccessTransformer.mergeWith(notfnl, DEFAULT, REMOVEFINAL), "removefinal+notfinal = notfinal"),
-                ()->assertEquals(fnl, AccessTransformer.mergeWith(fnl, DEFAULT, LEAVE), "leave+final = final"),
-                ()->assertEquals(notfnl, AccessTransformer.mergeWith(notfnl, DEFAULT, LEAVE), "leave+notfinal = notfinal")
+                () -> assertEquals(fnl, AccessTransformer.mergeWith(fnl, DEFAULT, MAKEFINAL), "makefinal+final = final"),
+                () -> assertEquals(fnl, AccessTransformer.mergeWith(notfnl, DEFAULT, MAKEFINAL), "makefinal+notfinal = final"),
+                () -> assertEquals(notfnl, AccessTransformer.mergeWith(fnl, DEFAULT, REMOVEFINAL), "removefinal+final = notfinal"),
+                () -> assertEquals(notfnl, AccessTransformer.mergeWith(notfnl, DEFAULT, REMOVEFINAL), "removefinal+notfinal = notfinal"),
+                () -> assertEquals(fnl, AccessTransformer.mergeWith(fnl, DEFAULT, LEAVE), "leave+final = final"),
+                () -> assertEquals(notfnl, AccessTransformer.mergeWith(notfnl, DEFAULT, LEAVE), "leave+notfinal = notfinal")
         );
     }
 }
--- src/test/java/net/neoforged/accesstransformer/test/TransformationTest.java
+++ src/test/java/net/neoforged/accesstransformer/test/TransformationTest.java
@@ -1,97 +1,196 @@
 package net.neoforged.accesstransformer.test;
 
 import net.neoforged.accesstransformer.api.AccessTransformerEngine;
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.core.config.Configurator;
-import org.junit.jupiter.api.BeforeAll;
+import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.Type;
 import org.objectweb.asm.tree.ClassNode;
 
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.StandardLocation;
+import javax.tools.ToolProvider;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringReader;
 import java.lang.reflect.Modifier;
+import java.net.URI;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.List;
+import java.util.Locale;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class TransformationTest {
-    private static final String testJarsPath = "build/classes/java/testJars";
+class TransformationTest {
+    @Test
+    public void testMemberTransformations() throws Throwable {
+        try (var cl = setupTest(
+                "ATTestClass.java",
+                """
+                        package net.neoforged.accesstransformer.testJar;
 
-    @BeforeAll
-    public static void setup() {
-        Configurator.setRootLevel(Level.DEBUG);
+                        public class ATTestClass {
+                            private final String finalPrivateField = "EMPTY";
+                            private String privateField = "EMPTY";
+
+                            private void privateMethod() {
+                            }
+
+                            public void otherMethod() {
+                                privateMethod();
+                            }
+                        }
+
+                        """, """
+                        public-f net.neoforged.accesstransformer.testJar.ATTestClass finalPrivateField
+                        protected net.neoforged.accesstransformer.testJar.ATTestClass privateField
+
+                        public net.neoforged.accesstransformer.testJar.ATTestClass privateMethod()V
+                        """
+        )) {
+            var transformedClass = Class.forName("net.neoforged.accesstransformer.testJar.ATTestClass", true, cl);
+            assertTrue(Modifier.isProtected(transformedClass.getDeclaredField("privateField").getModifiers()), "public field");
+
+            assertTrue(Modifier.isPublic(transformedClass.getDeclaredField("finalPrivateField").getModifiers()), "public field");
+            assertFalse(Modifier.isFinal(transformedClass.getDeclaredField("finalPrivateField").getModifiers()), "nonfinal field");
+
+            assertTrue(Modifier.isPublic(transformedClass.getDeclaredMethod("privateMethod").getModifiers()), "nonfinal method");
+        }
     }
 
     @Test
-    public void testTransformation(@TempDir Path tempDir) throws Throwable {
-        // Process the testJars
-        var engine = AccessTransformerEngine.newEngine();
-        engine.loadATFromResource("test_at.cfg");
+    public void testClassTransformation() throws Throwable {
+        try (var cl = setupTest(
+                "DefaultClass.java",
+                """
+                        package net.neoforged.accesstransformer.testJar;
 
-        var testJarsFolder = Paths.get(testJarsPath).toAbsolutePath();
-        var transformedJar = tempDir.resolve("testJars_transformed.jar");
-        try (var stream = Files.walk(testJarsFolder);
-                var os = Files.newOutputStream(transformedJar);
-                var zos = new ZipOutputStream(os)) {
-            stream.forEach(p -> {
-                if (Files.isDirectory(p)) {
-                    return;
-                }
+                        class DefaultClass {
 
-                var relativePath = testJarsFolder.relativize(p).toString();
+                            static class Inner {
+                            }
 
-                // Read
-                byte[] inputBytes;
-                try {
-                    inputBytes = Files.readAllBytes(p);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                ClassReader cr = new ClassReader(inputBytes);
-                ClassNode cn = new ClassNode();
-                cr.accept(cn, 0);
-                // Transform
-                var type = Type.getObjectType(cn.name.replace('.', '/'));
-                boolean transformed = engine.transform(cn, type);
-                assertTrue(transformed);
-                // Write
-                var cw = new ClassWriter(cr, 0);
-                cn.accept(cw);
-                try {
-                    zos.putNextEntry(new ZipEntry(relativePath.replace('\\', '/')));
-                    zos.write(cw.toByteArray());
-                    zos.closeEntry();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
+                        }
+                        """,
+                """
+                        public net.neoforged.accesstransformer.testJar.DefaultClass
+                        public net.neoforged.accesstransformer.testJar.DefaultClass$Inner
+                        """
+        )) {
+            var outer = Class.forName("net.neoforged.accesstransformer.testJar.DefaultClass", true, cl);
+            var inner = Class.forName("net.neoforged.accesstransformer.testJar.DefaultClass$Inner", true, cl);
+
+            assertTrue(Modifier.isPublic(outer.getModifiers()), "public class");
+            assertTrue(Modifier.isPublic(inner.getModifiers()), "public inner class");
+        }
+    }
+
+    /**
+     * Validate that the INNERCLASS metadata is correctly updated on the outer class.
+     */
+    @Test
+    public void testInnerClassModifierChangedOnParent() throws Throwable {
+        try (var cl = setupTest(
+                "OuterClass.java",
+                """
+                        package net.neoforged.accesstransformer.testJar;
+
+                        class OuterClass {
+
+                            static class Inner {
+                            }
+
+                        }
+                        """,
+                """
+                        public net.neoforged.accesstransformer.testJar.OuterClass$Inner
+                        """
+        )) {
+            var outer = Class.forName("net.neoforged.accesstransformer.testJar.OuterClass", true, cl);
+            var inner = Class.forName("net.neoforged.accesstransformer.testJar.OuterClass$Inner", true, cl);
+
+            assertFalse(Modifier.isPublic(outer.getModifiers()) || Modifier.isPrivate(outer.getModifiers()), "expected no changes on outer class modifiers");
+            assertTrue(Modifier.isPublic(inner.getModifiers()), "expected inner class to be made public");
+
+            final ClassNode outerNode = new ClassNode();
+            try (InputStream is = Files.newInputStream(classOutputDir.resolve(outer.getName().replace('.', '/') + ".class"))) {
+                final ClassReader classReader = new ClassReader(is);
+                classReader.accept(outerNode, 0);
+            }
+            outerNode.innerClasses.forEach(node -> {
+                assertTrue((node.access & Opcodes.ACC_PUBLIC) != 0);
             });
         }
+    }
 
-        // Shove the transformed jar into a URLClassLoader, and then run some tests on the loaded classes
-        var testJarsUrl = new URL[] { transformedJar.toUri().toURL() };
-        try (var testJarsClassLoader = new URLClassLoader(testJarsUrl, TransformationTest.class.getClassLoader())) {
-            var transformedClass = Class.forName("net.neoforged.accesstransformer.testJar.ATTestClass", true, testJarsClassLoader);
-            var transformedClass2 = Class.forName("net.neoforged.accesstransformer.testJar.DefaultClass", true, testJarsClassLoader);
-            var transformedClass3 = Class.forName("net.neoforged.accesstransformer.testJar.DefaultClass$Inner", true, testJarsClassLoader);
+    @TempDir
+    Path classOutputDir;
 
-            assertAll(
-                    () -> assertTrue(Modifier.isPublic(transformedClass2.getModifiers()), "public class"),
-                    () -> assertTrue(Modifier.isPublic(transformedClass3.getModifiers()), "public inner class"),
-                    () -> assertTrue(Modifier.isProtected(transformedClass.getDeclaredField("privateField").getModifiers()), "public field"),
-                    () -> assertTrue(Modifier.isPublic(transformedClass.getDeclaredField("finalPrivateField").getModifiers()), "public field"),
-                    () -> assertTrue(!Modifier.isFinal(transformedClass.getDeclaredField("finalPrivateField").getModifiers()), "nonfinal field"),
-                    () -> assertTrue(Modifier.isPublic(transformedClass.getDeclaredMethod("privateMethod").getModifiers()), "nonfinal method")
-            );
+    private URLClassLoader setupTest(String fileName, @Language("java") String source, String accessTransformerFile) throws IOException {
+        // First compile the source class
+        var diagnostics = new DiagnosticCollector<>();
+        var compiler = ToolProvider.getSystemJavaCompiler();
+        var manager = compiler.getStandardFileManager(diagnostics, Locale.ROOT, StandardCharsets.UTF_8);
+        manager.setLocationFromPaths(StandardLocation.CLASS_OUTPUT, List.of(classOutputDir));
+        var task = compiler.getTask(
+                null, manager, diagnostics,
+                List.of("-proc:none"), null,
+                List.of(new SimpleJavaFileObject(URI.create("string:///" + fileName), JavaFileObject.Kind.SOURCE) {
+                    @Override
+                    public CharSequence getCharContent(boolean ignoreEncodingErrors) {
+                        return source;
+                    }
+                })
+        );
+        if (!task.call()) {
+            diagnostics.getDiagnostics().forEach(diagnostic -> System.err.println("Failed to compile: " + diagnostic));
+            throw new RuntimeException("Failed to compile class");
         }
+
+        var engine = AccessTransformerEngine.newEngine();
+        engine.loadAT(new StringReader(accessTransformerFile), "test.accesstransformer");
+
+        // Then have the engine process each compiled class
+        Files.walkFileTree(classOutputDir, new SimpleFileVisitor<>() {
+            @Override
+            public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+                if (file.toString().endsWith(".class")) {
+                    // Read the class
+                    var inputBytes = Files.readAllBytes(file);
+                    var reader = new ClassReader(inputBytes);
+                    var node = new ClassNode();
+                    reader.accept(node, 0);
+
+                    // Transform the class
+                    var type = Type.getObjectType(node.name.replace('.', '/'));
+                    boolean transformed = engine.transform(node, type);
+
+                    // We expect each test class to be transformed
+                    assertTrue(transformed, "Expected class named " + node.name + " to be transformed by an access transformer");
+
+                    // Write the class back but modified
+                    var cw = new ClassWriter(reader, 0);
+                    node.accept(cw);
+                    Files.write(file, cw.toByteArray());
+                }
+                return FileVisitResult.CONTINUE;
+            }
+        });
+
+        // And finally create a class loader for the compiled files
+        return new URLClassLoader(new URL[] {classOutputDir.toUri().toURL()}, TransformationTest.class.getClassLoader());
     }
 }
